77d446729bb0821e767dfdaec1c4a9e05297d095
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-04-11 00:26:44 +0900

    Create Main_백준_15649_N과M_2_실3_추준성_88ms.java

---
+++ SSAFY_Algorithm/BOJ/백트래킹/Main_백준_15649_N과M_2_실3_추준성_88ms.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_15649_N과M_2_실3_추준성_88ms {
+	private static int N;
+	private static int M;
+	private static int[] output;
+	private static boolean[] visited;
+	private static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[N+1];
+		output = new int[M];
+		
+		perm(0);
+		
+		System.out.print(sb.toString());
+	} // end of main
+	
+	static void perm(int depth) {
+		if(depth == M) {
+			// 오름차순 아니면 리턴
+			for (int i = 0; i < M-1; i++) {
+				if(output[i] > output[i+1]) return;
+			}
+			
+			for (int i = 0; i < M; i++) {
+				sb.append(output[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if(visited[i]) continue;
+			output[depth] = i;
+			visited[i] = true;
+			perm(depth+1);
+			visited[i] = false;
+		}
+	} // end of method perm
+	
+
+} // end of class
