29daf901a1e250d618a52d1febb5b28f08ef2f34
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-04-11 01:31:23 +0900

    Create Main_백준_15665_N과M_11_실2_추준성_1112ms.java

---
+++ SSAFY_Algorithm/BOJ/백트래킹/Main_백준_15665_N과M_11_실2_추준성_1112ms.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main_백준_15665_N과M_11_실2_추준성_1112ms {
+	private static int N;
+	private static int M;
+	private static int[] input;
+	private static int[] output;
+	private static StringBuilder sb;
+	private static HashSet<String> set;
+
+	/*
+	 * 중복 결과가 출력되지 않게끔 해싱기법 활용 (HashSet 활용)
+	 */
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new int[N];
+		output = new int[M];
+		set = new HashSet<String>();
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input); // 사전 순으로 증가하는 순서로 수열을 출력하기 위해 정렬
+		
+		perm(0);
+		
+		System.out.print(sb.toString());
+	} // end of main
+	
+	static void perm(int depth) {
+		if(depth == M) {
+			
+			// 해싱 기법 (중복 결과가 출력 안 되게끔)
+			StringBuilder strb = new StringBuilder();
+			for (int i = 0; i < M; i++) {
+				strb.append(output[i]);
+			}
+			String s = strb.toString();
+			
+			if(set.contains(s)) return; // 기존에 추가된 수열이면 출력 안 하고 리턴
+			set.add(s); // 아니면 추가
+			
+			// 결과 출력
+			for (int i = 0; i < M; i++) {
+				sb.append(output[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			output[depth] = input[i];
+			perm(depth+1);
+		}
+	} // end of method perm
+	
+
+} // end of class
