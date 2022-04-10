83c7f71c2a5cc3bd41b7df942cba850fadc30056
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-04-11 00:43:36 +0900

    Create Main_백준_15656_N과M_7_실3_추준성_604ms.java

---
+++ SSAFY_Algorithm/BOJ/백트래킹/Main_백준_15656_N과M_7_실3_추준성_604ms.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_백준_15656_N과M_7_실3_추준성_604ms {
+	private static int N;
+	private static int M;
+	private static int[] input;
+	private static int[] output;
+	private static StringBuilder sb;
+
+	/*
+	 * 1. 중복순열
+	 */
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new int[N+1];
+		output = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input); // 사전 순으로 증가하는 순서로 수열을 출력하기 위해 정렬
+		
+		perm(0);
+		
+		System.out.print(sb.toString());
+	} // end of main
+	
+	static void perm(int depth) {
+		if(depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(output[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			output[depth] = input[i];
+			perm(depth+1);
+		}
+	} // end of method perm
+	
+
+} // end of class
