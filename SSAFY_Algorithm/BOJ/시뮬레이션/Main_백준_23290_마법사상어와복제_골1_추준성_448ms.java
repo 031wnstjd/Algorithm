1b846bbe60dba0d5f86446b8ea3c255c04dc1ba3
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-04-28 00:59:56 +0900

    Create Main_백준_23290_마법사상어와복제_골1_추준성_448ms.java

---
+++ SSAFY_Algorithm/BOJ/시뮬레이션/Main_백준_23290_마법사상어와복제_골1_추준성_448ms.java
@@ -0,0 +1,222 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main_백준_23290_마법사상어와복제_골1_추준성_448ms {
+	/*
+	 * <조건>
+	 * 1. 둘 이상의 물고기가 같은 칸에 있을 수도 있으며, 마법사 상어와 물고기가 같은 칸에 있을 수도 있다.
+	 * 2. 복제 마법은 시간이 조금 걸리기 때문에, 아래 5번에서 물고기가 복제되어 칸에 나타난다.
+	 * 
+	 * -물고기 이동 (8방)
+	 * 3. 모든 물고기가 한 칸 이동한다. 상어가 있는 칸, 물고기의 냄새가 있는 칸, 격자의 범위를 벗어나는 칸으로는 이동할 수 없다.
+	 * 4. 각 물고기는 자신이 가지고 있는 이동 방향이 이동할 수 있는 칸을 향할 때까지 방향을 '45도 반시계 회전'시킨다. 
+	 * 5. 만약, 이동할 수 있는 칸이 없으면 이동을 하지 않는다.
+	 * 
+	 * - 상어 이동 (4방)
+	 * 6. 상어가 연속해서 3칸 이동한다. 
+	 * 7. 연속해서 이동하는 칸 중에 격자의 범위를 벗어나는 칸이 있으면, 그 방법은 불가능한 이동 방법이다. (이동 가능 체크가 우선)
+	 * 8. 연속해서 이동하는 중에 상어가 물고기가 있는 같은 칸으로 이동하게 된다면, 그 칸에 있는 모든 물고기는 격자에서 제외되며, 제외되는 모든 물고기는 물고기 냄새를 남긴다.
+	 * 9. 가능한 이동 방법 중에서 제외되는 물고기의 수가 가장 많은 방법으로 이동
+	 * 10. 그러한 방법이 여러가지인 경우 사전 순으로 가장 앞서는 방법을 이용 (노트참고)
+	 * 11. 두 번 전 연습에서 생긴 물고기의 냄새가 격자에서 사라진다. (냄새 class의 시간 요소 추가)
+	 * 12. 복제 마법이 완료된다. 모든 복제된 물고기는 1에서의 위치와 방향을 그대로 갖게 된다.(tmp[][] 배열에 저장해놓고 이후 업데이트)
+	 * 
+	 */
+	
+	static int[] shkdr = {-1, 0, 1, 0}; // 상 좌 하 우
+	static int[] shkdc = {0,-1, 0, 1};
+	
+	static int[] fshdr = {0,-1,-1,-1, 0, 1, 1, 1}; 
+	static int[] fshdc = {-1,-1, 0, 1, 1, 1, 0,-1};
+	private static ArrayList<Fish>[][] fishMap;
+	private static ArrayList<Fish> copyList;
+	private static ArrayList<Fish> fishList;
+	private static int maxFeed;
+	private static int[] maxFeedDir;
+	private static Shark shark;
+	private static int[][] smellMap;
+	
+	static class Shark {
+		int r;
+		int c;
+		public Shark(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	static class Fish implements Cloneable{
+		int r;
+		int c;
+		int d;
+		public Fish(int r, int c, int d) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.d = d;
+		}
+		@Override
+		protected Fish clone() throws CloneNotSupportedException {
+			return (Fish) super.clone();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		fishList = new ArrayList<>();
+		copyList = new ArrayList<>();
+		smellMap = new int[4][4]; // 냄새 표시 (값 == 생성 회차)
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int fr = Integer.parseInt(st.nextToken()) - 1;
+			int fc = Integer.parseInt(st.nextToken()) - 1;
+			int fd = Integer.parseInt(st.nextToken()) - 1; // 물고기 방향
+			fishList.add(new Fish(fr, fc, fd));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int sr = Integer.parseInt(st.nextToken()) - 1;
+		int sc = Integer.parseInt(st.nextToken()) - 1;
+		shark = new Shark(sr, sc); // shark는 한 마리이므로 한 객체만으로 위치 업데이트
+		
+		fishMap = new ArrayList[4][4];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				fishMap[i][j] = new ArrayList<Fish>();
+			}
+		}
+		
+		// S번 시행
+		for (int step = 1; step <= S; step++) {
+			copyFish(); // 초기 물고기 복사
+			moveFish(); // 물고기 이동
+			moveShark(step); // 상어 이동
+			removeSmell(step); // 냄새 제거
+			copyMagic(); // 복제 마법 적용
+			resetMap();
+		} // end of for S
+			
+		System.out.print(fishList.size());
+	} // end of main
+	
+	static void copyFish() throws CloneNotSupportedException {
+		for (Fish fish : fishList) {
+			copyList.add(fish.clone());
+		}
+	}
+	
+	static void moveFish() {
+		for (Fish fish : fishList) {
+			int r = fish.r;
+			int c = fish.c;
+			int d = fish.d;
+					
+			boolean isMove = false;
+			for (int i = 0; i < 8; i++) {
+				if(d == -1) d = 7;
+				int nr = r + fshdr[d];
+				int nc = c + fshdc[d];
+				if(0 <= nr && nr < 4 && 0 <= nc && nc < 4 && smellMap[nr][nc] == 0 && (nr != shark.r || nc != shark.c)) {
+					fish.r = nr;
+					fish.c = nc;
+					fish.d = d;
+					fishMap[nr][nc].add(fish); // map 업데이트
+					isMove = true;
+					break;
+				}
+				d--;
+			}
+			if(!isMove) fishMap[r][c].add(fish);
+		}
+	}
+	
+	static void moveShark(int step) {
+		int[] selected = new int[3];
+		maxFeed = -1;
+		maxFeedDir = new int[3]; // 최대 먹이를 먹을 수 있는 위치 정보 저장
+		setMaxFeedDir(0, selected);
+		int nr = shark.r;
+		int nc = shark.c;
+		for (int i = 0; i < 3; i++) { // 물고기 제거 & 냄새 남기기
+			nr += shkdr[maxFeedDir[i]];
+			nc += shkdc[maxFeedDir[i]];
+			
+			if(fishMap[nr][nc].size() != 0) {
+				fishMap[nr][nc].clear(); // 해당위치 물고기 모두 삭제
+				smellMap[nr][nc] = step; // 냄새 남기기
+			}
+		}
+		shark.r = nr; // 상어 이동
+		shark.c = nc;
+	}
+	
+	static void setMaxFeedDir(int depth, int[] selected) {
+		if(depth == 3) {
+			int fishCnt = 0; // 먹은 물고기의 수 초기화
+			int nr = shark.r; // 현재 상어 위치 초기화
+			int nc = shark.c;
+			int[] tmpDir = new int[3]; 
+			boolean[][] visited = new boolean[4][4];
+			for (int i = 0; i < 3; i++) {
+				int setDir = selected[i];
+				nr += shkdr[setDir];
+				nc += shkdc[setDir];
+				
+				if(nr < 0 || nr >= 4 || nc < 0 || nc >= 4) return; // 범위 밖으로 나가면 리턴 (이동 불가)
+				
+				if(!visited[nr][nc]) fishCnt += fishMap[nr][nc].size(); // 해당 위치에 있는 물고기 모두 먹기
+				
+				visited[nr][nc] = true;
+				tmpDir[i] = setDir;
+			}
+			if(maxFeed < fishCnt) { // 물고기를 기존 최대 보다 많이 먹을 때 갱신
+				maxFeed = fishCnt;
+				for (int i = 0; i < 3; i++) {
+					maxFeedDir[i] = tmpDir[i]; // 최대 먹이를 먹을 수 있는 방향 정보를 갱신/저장
+				}
+			}
+			return;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			selected[depth] = i;
+			setMaxFeedDir(depth+1, selected);
+		}
+	}
+	
+	static void removeSmell(int step) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if(smellMap[i][j] == 0) continue;
+				if(step == smellMap[i][j] + 2) smellMap[i][j] = 0;
+			}
+		}
+	}
+	
+	static void copyMagic() {
+		for (Fish fish : copyList) {
+			fishMap[fish.r][fish.c].add(fish);
+		}
+		copyList.clear();
+	}
+	
+	static void resetMap() {
+		fishList.clear();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (Fish fish : fishMap[i][j]) {
+					fishList.add(fish);
+				}
+				fishMap[i][j].clear();
+			}
+		}
+	}
+	
+} // end of class
