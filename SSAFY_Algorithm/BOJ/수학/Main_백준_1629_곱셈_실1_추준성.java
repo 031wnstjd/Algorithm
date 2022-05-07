e3bf9c6a61a534bbc13df7a0518397f145835685
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-05-07 17:32:50 +0900

    Create Main_백준_1629_곱셈_실1_추준성.java

---
+++ SSAFY_Algorithm/BOJ/수학/Main_백준_1629_곱셈_실1_추준성.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_1629_곱셈_실1_추준성 {
+	/*
+	 * 모듈러 연산 : (a*b)%c == ((a%c)*(b%c))%c
+	 */
+	private static int c;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		System.out.println(pow(a, b));
+	} // end of main
+
+	private static long pow(long a, long b) {
+		if(b == 1) return a % c;
+		
+		long tmp = pow(a, b>>1);
+		
+		if(b%2==0) return tmp * tmp % c;
+		else return (tmp * tmp % c) * a % c;
+	}
+	
+} // end of class
