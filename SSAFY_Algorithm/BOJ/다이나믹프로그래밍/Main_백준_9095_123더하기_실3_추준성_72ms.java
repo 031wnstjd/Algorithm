d3dad489b44483c177497ff4758f2af20e28cc63
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-06-08 13:05:21 +0900

    Update and rename Main_백준_9095_123더하기_실3_추준성_76ms.java to Main_백준_9095_123더하기_실3_추준성_72ms.java

--- SSAFY_Algorithm/BOJ/다이나믹프로그래밍/Main_백준_9095_123더하기_실3_추준성_72ms.java
+++ SSAFY_Algorithm/BOJ/다이나믹프로그래밍/Main_백준_9095_123더하기_실3_추준성_72ms.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main_백준_9095_123더하기_실3_추준성_76ms {
-	
+public class Main_백준_9095_123더하기_실3_추준성_72ms {
+	/*
+	 * 최적 부분 구조 & 하위 문제 중복 => DP
+	 */
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int tc = Integer.parseInt(br.readLine());
+		int[] dp = new int[11];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+		
 		for (int testCase = 0; testCase < tc; testCase++) {
 			int n = Integer.parseInt(br.readLine());
-			
-			if(n <= 3) {
-				if(n == 1) sb.append(1).append("\n");
-				if(n == 2) sb.append(2).append("\n");
-				if(n == 3) sb.append(4).append("\n");
-				continue;
-			}
-			
-			int[] dp = new int[n+1];
-			
-			dp[1] = 1;
-			dp[2] = 2;
-			dp[3] = 4;
-			
-			for (int i = 4; i <= n; i++) {
-				dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-			}
-			
 			sb.append(dp[n]).append("\n");
-			
 		} // end of for testCase
 		
 		System.out.print(sb.toString());
