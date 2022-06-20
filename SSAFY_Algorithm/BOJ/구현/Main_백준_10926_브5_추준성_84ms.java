d416e8c5d6d95050ab22c3d866aa5c5495540cb4
추준성 <55497825+031wnstjd@users.noreply.github.com>
2022-06-20 23:58:44 +0900

    Create Main_백준_10926_브5_추준성_84ms.java

---
+++ SSAFY_Algorithm/BOJ/구현/Main_백준_10926_브5_추준성_84ms.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_백준_10926_브5_추준성_84ms {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.println(str+"??!");
+		
+	} // end of main
+
+} // end of class
